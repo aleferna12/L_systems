b13cc36f4cb017d3146f36071140e5986ce579dc
aleferna12 <64274141+aleferna12@users.noreply.github.com>
2024-09-17 00:05:14 +0100

    TODOS in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include "parameters.h"
 #include "model.h"
 
+// TODO: add a second terminal gene representing a leaf that determines how fast the plant develops (get rid of synchronous calls to 'tree.develop')
+//       rewrite in rust for fun
+//       make genes their own struct to simplify the mess with gene ids etc
+
 int main() {
     Model model(Parameters {});
     model.run();
