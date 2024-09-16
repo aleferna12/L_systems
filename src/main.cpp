57d470cdda12689f6573167c8f5aeab22a615751
aleferna12 <64274141+aleferna12@users.noreply.github.com>
2024-09-17 00:42:09 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include "parameters.h"
 #include "model.h"
 
-// TODO: add a second terminal gene representing a leaf that determines how fast the plant develops (get rid of synchronous calls to 'tree.develop')
-//       rewrite in rust for fun
+// TODO: rewrite in rust for fun
 //       make genes their own struct to simplify the mess with gene ids etc
+//       add a second terminal gene representing a leaf that determines how fast the plant develops (get rid of synchronous calls to 'tree.develop')
+//       add grid structure to forest and rework IO to get OBJ files of the whole forest
 
 int main() {
     Model model(Parameters {});
