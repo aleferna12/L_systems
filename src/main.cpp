7c44d72ca9f300ef4cc884dd6068cbeca2c3d92d
aleferna12 <aleferna2001@hotmail.com>
2024-09-19 22:11:50 +0100

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "parameters.h"
 #include "model.h"
 
-// TODO: rewrite in rust for fun
+// TODO: fixing growth logic broke the whole model, we need to make sure tree cant grow downwards
+//       rewrite in rust for fun
 //       make genes their own struct to simplify the mess with gene ids etc
 //       add a second terminal gene representing a leaf that determines how fast the plant develops (get rid of synchronous calls to 'tree.develop')
 //       add grid structure to forest and rework IO to get OBJ files of the whole forest
