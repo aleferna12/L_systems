aff2df62c73764f8a56e726fb3b81e95196204e9
aleferna <aleferna2001@hotmail.com>
2025-04-08 21:05:52 +0100

    Compiled own project with pico dependencies

---
+++ src/graphics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alefe on 08/04/2025.
+//
+
+#include "graphics.h"
---
+++ src/graphics.h
@@ -0,0 +1,17 @@
+//
+// Created by alefe on 08/04/2025.
+//
+
+#ifndef L_SYSTEMS_GRAPHICS_H
+#define L_SYSTEMS_GRAPHICS_H
+
+#include "libraries/pico_display_2/pico_display_2.hpp"
+#include "drivers/st7789/st7789.hpp"
+#include "libraries/pico_graphics/pico_graphics.hpp"
+
+class Graphics {
+
+};
+
+
+#endif //L_SYSTEMS_GRAPHICS_H
